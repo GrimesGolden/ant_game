47213ba0599904187ca425ab106f5af75464140f
Jordan Tobin <jordantobin123@hotmail.com>
2025-11-06 17:03:52 -0800

    Enemy AI functional and shoot mechanism functional

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    const int startingHealth = 100;
+    int currentHealth;
+
+    void Awake()
+    {   
+        // Initialize health. 
+        currentHealth = startingHealth; 
+    }
+
+    void CheckHealth()
+    {   
+        // Simply check if health is less than zero and destroy if true. 
+        if(currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    public void TakeDamage(int damageAmount)
+    {
+        // Public to be called by weapons script (or others)
+        currentHealth -= damageAmount;
+        Debug.Log("Blammo, direct hit, remaining enemy health is: " + currentHealth);
+        CheckHealth(); 
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,32 +4,51 @@ using StarterAssets;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
-{   
-    StarterAssetsInputs starterAssetsInputs; 
+{
+    StarterAssetsInputs starterAssetsInputs;
+ //   EnemyHealth enemyHealth;
     void Awake()
-    {   
+    {
         // Get this main script from the parent (player)
         // This starter assets script is created by the FirstPersonController, a controller given by unity for moduler input binding. 
-        starterAssetsInputs = gameObject.GetComponentInParent<StarterAssetsInputs>(); 
+        starterAssetsInputs = gameObject.GetComponentInParent<StarterAssetsInputs>();
     }
+    
+    //void Start()
+   // {   
+        // One way of getting the script, though you could also just serialize. 
+    //    enemyHealth = GameObject.FindAnyObjectByType<Robot>().GetComponent<EnemyHealth>(); 
+  //  }
     void Update()
     {
-        Shoot(); 
+        HandleShoot(); 
     }
 
-    void Shoot()
-    {   
+    void HandleShoot()
+    {
         // Use the cameras position (reference is precached by unity, no need to save variable)
         // Then cast in the forward direction from the camera
         // Use out keyword with raycast hit
         // Raycast will travel for a distance of Infinity (essentially no distance cap)
 
-        RaycastHit hit;
+        // Note that RayCastHit will only return a value if we hit a collider. This prevents null ref
 
-        if (starterAssetsInputs.shoot && Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
-        {
-            Debug.Log(hit.collider.name);
+        RaycastHit hit;
+        const int damageAmount = 45; 
+
+        if(!starterAssetsInputs.shoot)
+        {   
+            // eliminate one indentation block
+            return; 
         }
-        starterAssetsInputs.ShootInput(false); // use the method to turn the public bool back to false. if you want to be super safe.
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity) && hit.collider.tag == "Enemy")
+        {   
+            // You could also just check that enemyHealth just returns null with if(enemyHealth) but I chose to use a tag. 
+            EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+            enemyHealth.TakeDamage(damageAmount);
+        }
+        starterAssetsInputs.ShootInput(false); // use the method to turn the public bool back to false. 
+        // could also just have gotten the public shoot bool and turned it false, but using the method is clearer. 
     }
 }
